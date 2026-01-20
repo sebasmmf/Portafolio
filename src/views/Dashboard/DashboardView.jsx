856d05f4bba0b23c4d043670b47cb799bb890bd0
Sebastian Martinez <sebastian.mart.f@gmail.com>
2026-01-20 01:30:32 -0400

    Design changes

--- src/views/Dashboard/DashboardView.jsx
+++ src/views/Dashboard/DashboardView.jsx
@@ -2,7 +2,6 @@ import MainSection from '../../sections/MainSection/MainSection';
 import ProjectsSection from '../../sections/ProjectsSection/ProjectsSection';
 import SkillsSection from '../../sections/SkillsSection/SkillsSection';
 import AboutSection from '../../sections/AboutSection/AboutSection';
-import './DashboardView.css'
 
 export default function DashboardView() {
 
